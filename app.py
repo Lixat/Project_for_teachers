9b5f97da89933738167b4cb040234a29dd10103d
Lixat <mr_dyman@mail.ru>
2020-12-30 20:17:59 +0200

    for checking

--- app.py
+++ app.py
@@ -1,51 +1,119 @@
-import json
+from json import load, dump
+from random import randint
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+week = {
+    'mon': 'Понедельник',
+    'tue': 'Вторник',
+    'wed': 'Среда',
+    'thu': 'Четверг',
+    'fri': 'Пятница',
+    'sat': 'Суббота',
+    'sun': 'Воскресенье'
+}
 
 
 @app.route('/')
 def index_view():
-    return 'Index page'
+    with open('db.json', 'r') as f:
+        data = load(f)
+    teachers = []
+    while len(teachers) < 6:
+        teacher = data['teachers'][randint(0, 11)]
+        if teacher not in teachers:
+            teachers.append(teacher)
+    goals = data['goals']
+    return render_template('index.html', teachers=teachers, goals=goals)
 
 
 @app.route('/all')
 def all_teachers_view():
-    return 'All teachers'
+    with open('db.json', 'r') as f:
+        data = load(f)
+    all = data['teachers']
+    return render_template('all.html', all=all)
 
 
 @app.route('/goals/<goal>/')
 def goal_view(goal):
-    return 'здесь будет цель <goal>'
+    with open('db.json', 'r') as f:
+        data = load(f)
+    teachers = data['teachers']
+    profile = [teacher for teacher in teachers if goal in teacher['goals']]
+    goals = data['goals']
+    return render_template('goal.html', profile=profile, goal=goals[goal])
 
 
 @app.route('/profiles/<int:id>/')
 def profile_view(id):
     with open('db.json', 'r', encoding='utf-8') as f:
-        profiles = json.load(f)
-    print(type(profiles))
-    return render_template('profile.html', id=id, profiles=profiles)
+        data = load(f)
+    profile = data['teachers'][id]
+    goals_for = data['goals']
+    return render_template('profile.html', profile=profile, goals_for=goals_for, week=week)
 
 
 @app.route('/request/')
 def request_view():
-    return 'здесь будет заявка на подбор'
+    with open('db.json', 'r') as f:
+        data = load(f)
+    goals = data['goals']
+    return render_template('request.html', goals=goals)
 
 
-@app.route('/request_done/')
+@app.route('/request_done/', methods=['POST'])
 def request_done_view():
-    return 'заявка на подбор отправлена'
+    goal = request.form['goal']
+    time = request.form['time']
+    name = request.form['username']
+    phone = request.form['userphone']
+    info = {
+        'goal': goal,
+        'time': time,
+        'name': name,
+        'phone': phone
+    }
+    with open('request.json', 'r') as f:
+        data = load(f)
+    data.append(info)
+    with open('request.json', 'w') as f:
+        dump(data, f)
+    return render_template('request_done.html', info=info)
 
 
 @app.route('/booking/<int:id>/<day>/<time>/')
-def booking_view():
-    return 'здесь будет форма бронирования <id учителя>'
+def booking_view(id, day, time):
+    with open('db.json', 'r') as f:
+        data = load(f)
+    profile = data['teachers'][id]
+    day = day
+    time = time
+    return render_template('booking.html', profile=profile, day=day, time=time, week=week)
 
 
-@app.route('/booking_done/')
+@app.route('/booking_done/', methods=['POST'])
 def booking_done_view():
-    return 'заявка отправлена'
+    id = request.form['clientTeacher']
+    day = request.form['clientWeekday']
+    time = request.form['clientTime']
+    client_name = request.form['clientName']
+    client_phone = request.form['clientPhone']
+    order = {
+        'id': id,
+        'day': day,
+        'time': time,
+        'client_name': client_name,
+        'client_phone': client_phone
+    }
+    with open('booking.json', 'r') as f:
+        data = load(f)
+    data.append(order)
+    with open('booking.json', 'w') as f:
+        dump(data, f)
+    return render_template('booking_done.html', order=order, week=week)
 
 
 if __name__ == '__main__':
--- data_to_db.py
+++ data_to_db.py
@@ -1,7 +1,10 @@
 import json
 
-with open('data.py', 'r') as f:
-    data = f.read()
+from data import goals, teachers
+
+data = {}
+data['goals'] = goals
+data['teachers'] = teachers
 
 with open('db.json', 'w', encoding='utf-8') as db:
     json.dump(data, db)
